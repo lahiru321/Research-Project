72634a613d5056afbb2a08885ecf07ec266fc11e
lahiru321 <lahirujeewantha321@gmail.com>
2024-10-01 19:01:19 +0530

    basic frontend part pages

---
+++ src/components/Academic.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Academic() {
+  return (
+    <div>
+      <h1>Academic Page</h1>
+      <p>Here is where academic-related content will be displayed.</p>
+    </div>
+  );
+}
+
+export default Academic;
---
+++ src/components/Financial.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Financial() {
+  return (
+    <div>
+      <h1>Financial Page</h1>
+      <p>This page focuses on financial topics.</p>
+    </div>
+  );
+}
+
+export default Financial;
